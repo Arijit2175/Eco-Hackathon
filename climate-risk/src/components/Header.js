92e81348a7eec6f26abe4bed74323145461afdeb
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-08-28 00:09:31 +0530

    added export lines

--- climate-risk/src/components/Header.js
+++ climate-risk/src/components/Header.js
@@ -1,3 +1,5 @@
 import React from "react";
 
-const Header = () => <header className="header"><div className="header-title"><SunIcon /><h1>ClimaSafe</h1></div></header>;
+const Header = () => <header className="header"><div className="header-title"><SunIcon /><h1>Climate-Safe</h1></div></header>;
+
+export default Header;
