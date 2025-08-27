d8f37b2cc64ff394b3f368700f60f087d2ed5e4d
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-08-28 00:11:28 +0530

    added import for sun icon

--- climate-risk/src/components/Header.js
+++ climate-risk/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { SunIcon } from "lucide-react";
 
 const Header = () => <header className="header"><div className="header-title"><SunIcon /><h1>Climate-Safe</h1></div></header>;
 
