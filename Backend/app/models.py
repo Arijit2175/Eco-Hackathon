dded7bbc9787bb503f47acd9d51603aecd15c9ad
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-09-11 10:25:19 +0530

    function for loading random forest model

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -5,3 +5,6 @@ import pandas as pd
 import tensorflow as tf
 from app.config import MODEL_DIR, PROCESSED_PATH
 
+def load_random_forest():
+    return joblib.load(os.path.join(MODEL_DIR, "rainfall_rf.pkl"))
+
