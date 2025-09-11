56b58b2e42d91863364149955efc262543a90bbb
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-09-11 10:21:29 +0530

    importing modules

---
+++ Backend/app/train_lstm.py
@@ -0,0 +1,9 @@
+import os
+import numpy as np
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.optimizers import Adam
+from sklearn.preprocessing import MinMaxScaler
+from app.config import PROCESSED_PATH, MODEL_DIR
+
