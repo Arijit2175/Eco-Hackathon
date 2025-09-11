a6d4b011b7c7393276a6f11d133e1d4d9ea03329
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-09-11 10:21:55 +0530

    function for training lstm model

--- Backend/app/train_lstm.py
+++ Backend/app/train_lstm.py
@@ -7,3 +7,30 @@ from tensorflow.keras.optimizers import Adam
 from sklearn.preprocessing import MinMaxScaler
 from app.config import PROCESSED_PATH, MODEL_DIR
 
+def train_lstm(seq_len=5):
+    df = pd.read_csv(PROCESSED_PATH)
+    features = ["temperature_c", "humidity", "wind_speed", "pressure", "temp_humidity_index"]
+    target = "rain"
+
+    scaler = MinMaxScaler()
+    X_scaled = scaler.fit_transform(df[features])
+    y = df[target].values
+
+    X_seq, y_seq = [], []
+    for i in range(len(X_scaled) - seq_len):
+        X_seq.append(X_scaled[i:i+seq_len])
+        y_seq.append(y[i+seq_len])
+    X_seq, y_seq = np.array(X_seq), np.array(y_seq)
+
+    model = Sequential([
+        LSTM(64, input_shape=(seq_len, len(features))),
+        Dense(1)
+    ])
+    model.compile(optimizer=Adam(0.001), loss="mse")
+    model.fit(X_seq, y_seq, epochs=10, batch_size=16, verbose=1)
+
+    os.makedirs(MODEL_DIR, exist_ok=True)
+    model.save(os.path.join(MODEL_DIR, "rainfall_lstm.h5"))
+
+    print("✅ LSTM model trained and saved.")
+
