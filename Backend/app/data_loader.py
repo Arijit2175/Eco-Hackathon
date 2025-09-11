7cef957c64917490a06a4f4896236a962711521b
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-09-11 09:39:54 +0530

    making paths for saving data

--- Backend/app/data_loader.py
+++ Backend/app/data_loader.py
@@ -3,3 +3,9 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 
+RAW_PATH = os.path.join("data", "raw")
+PROCESSED_PATH = os.path.join("data", "processed")
+
+os.makedirs(RAW_PATH, exist_ok=True)
+os.makedirs(PROCESSED_PATH, exist_ok=True)
+
