7cdff6726311ba339fe2b38aa2d4dc6b85e30c00
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-09-11 20:15:06 +0530

    added load model

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -3,7 +3,8 @@ import joblib
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from app.config import MODEL_DIR, PROCESSED_PATH
+from tensorflow.keras.models import load_model
+from app.config import MODEL_DIR
 
 def load_random_forest():
     return joblib.load(os.path.join(MODEL_DIR, "rainfall_rf.pkl"))
