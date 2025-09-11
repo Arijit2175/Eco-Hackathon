7f23f6455bf61a6ee8bd2935468dceced63f7218
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-09-11 20:21:57 +0530

    driver code

--- Backend/app/api.py
+++ Backend/app/api.py
@@ -27,3 +27,5 @@ def predict_temperature(data: TemperatureRequest):
     pred = predict_temperature_lstm(data.sequence)
     return {"predicted_temperature_c": pred}
 
+if __name__ == "__main__":
+    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
