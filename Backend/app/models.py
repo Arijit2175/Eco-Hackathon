7954b4115d972a4beaa2ff708a5108ab7fc6c31e
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-09-11 10:26:01 +0530

    inference layer

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -11,3 +11,11 @@ def load_random_forest():
 def load_lstm():
     return tf.keras.models.load_model(os.path.join(MODEL_DIR, "rainfall_lstm.h5"))
 
+def predict_rainfall_rf(features: pd.DataFrame):
+    model = load_random_forest()
+    return model.predict(features)
+
+def predict_rainfall_lstm(sequence: np.ndarray):
+    model = load_lstm()
+    seq = np.expand_dims(sequence, axis=0)  
+    return model.predict(seq).flatten()[0]
