abaee2fdb52952cce1f3e30f341631905f36f0ef
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-09-11 10:03:18 +0530

    added env loading

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY", "")
 ONECALL_URL = "https://api.openweathermap.org/data/2.5/onecall"
