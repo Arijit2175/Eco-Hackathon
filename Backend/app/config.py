d2d6d7f51e62dce8447ab90b3b3929f5a748fd1d
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-09-11 10:19:45 +0530

    added directories

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -9,3 +9,9 @@ AIR_POLLUTION_URL = "http://api.openweathermap.org/data/2.5/air_pollution"
 
 if not OPENWEATHER_API_KEY:
     raise ValueError("Missing OpenWeather API key. Set OPENWEATHER_API_KEY env variable.")
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+RAW_PATH = os.path.join(DATA_DIR, "raw", "climate_data_raw.csv")
+PROCESSED_PATH = os.path.join(DATA_DIR, "processed", "climate_data_processed.csv")
+MODEL_DIR = os.path.join(BASE_DIR, "models")
