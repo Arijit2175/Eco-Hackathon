8dd3e93ee11f4c44e7ae198a7d923f7a701f817c
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-09-11 09:43:30 +0530

    function for preprocessing

--- Backend/app/features.py
+++ Backend/app/features.py
@@ -3,3 +3,8 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from app.data_loader import RAW_PATH, PROCESSED_PATH
 
+def preprocess_data():
+    file = os.path.join(RAW_PATH, "climate_data.csv")
+    df = pd.read_csv(file, parse_dates=["date"])
+
+    
