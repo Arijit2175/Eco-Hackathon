10bf9659be3523426646836fc3b5101a380176b3
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-09-13 10:03:09 +0530

    rechanging frontend

--- climate-risk/src/components/Header.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { SunIcon } from "../assets/icons";
-
-const Header = () => <header className="header"><div className="header-title"><SunIcon /><h1>Climate-Safe</h1></div></header>;
-
-export default Header;
